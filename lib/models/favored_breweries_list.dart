8a1adb405dfc61665c10b13ec6bc66b61f4a4f5b
Ori Baram <obrm770@gmail.com>
2022-11-27 09:40:58 +0200

    refactoring

--- lib/models/favored_breweries_list.dart
+++ lib/models/favored_breweries_list.dart
@@ -2,10 +2,4 @@ import 'package:my_breweries/models/brewery.dart';
 
 class FavoredBreweriesList {
   List<Brewery> list = [];
-
-  toJSONEncodable() {
-    return list.map((item) {
-      return item.toJson();
-    }).toList();
-  }
 }
